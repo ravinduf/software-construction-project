97f035faf8f6779e6324e4ff1b54cf5d66cb12c5
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-14 22:42:45 +0530

    add varible to get mysql connection

--- src/main/java/emailRepository/MysqlEmailRepository.java
+++ src/main/java/emailRepository/MysqlEmailRepository.java
@@ -1,8 +1,16 @@
 package emailRepository;
 
+import java.sql.Connection;
 import java.util.ArrayList;
 
 public class MysqlEmailRepository implements EmailRepository {
+    Connection con;
+    String user = "root";
+    String password = "";
+    String url = "jdbc:mysql://localhost:3306/emails";
+    String quary = "SELECT useremail FROM email";
+    ArrayList<String> userData = new ArrayList<String>();
+    
     public ArrayList<String> ReadOperation(){
         return null;
     }
