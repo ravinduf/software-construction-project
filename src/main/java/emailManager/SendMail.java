35b7b7d0fc59ab161dfb0819ffb611f16ce43886
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-14 21:55:03 +0530

    add function operate that passed email and error messages

--- src/main/java/emailManager/SendMail.java
+++ src/main/java/emailManager/SendMail.java
@@ -1,4 +1,10 @@
 package emailManager;
 
+import java.util.ArrayList;
+
 public class SendMail {
+    public ArrayList<String> EmailSendRepository(ArrayList<String> emails, String errorMessage) {
+
+        return null;
+    }
 }
