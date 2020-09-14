c4f665ef46e4bc95f8b903f66142537424194de1
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-14 22:44:30 +0530

    get mysql database connection

--- src/main/java/emailRepository/MysqlEmailRepository.java
+++ src/main/java/emailRepository/MysqlEmailRepository.java
@@ -1,6 +1,8 @@
 package emailRepository;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class MysqlEmailRepository implements EmailRepository {
@@ -10,7 +12,16 @@ public class MysqlEmailRepository implements EmailRepository {
     String url = "jdbc:mysql://localhost:3306/emails";
     String quary = "SELECT useremail FROM email";
     ArrayList<String> userData = new ArrayList<String>();
-    
+
+    public MysqlEmailRepository() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/emails?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", user, password);
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
     public ArrayList<String> ReadOperation(){
         return null;
     }
