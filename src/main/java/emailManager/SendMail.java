2b7a0035305dffafeb66ed36f1a448895e62d894
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-14 22:21:11 +0530

    add operation to call sendMail class and pass emails to that class

--- src/main/java/emailManager/SendMail.java
+++ src/main/java/emailManager/SendMail.java
@@ -1,11 +1,23 @@
 package emailManager;
 
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 import java.util.ArrayList;
 
 public class SendMail {
     public ArrayList<String> EmailSendRepository(ArrayList<String> emails, String errorMessage) {
         if ( errorMessage != null ) {
-            
+            for (String email: emails ){
+                try {
+                    SendMail mailSend = new SendMail();
+                    JsonNode jsonNode = mailSend.sendSimpleMessage(email, errorMessage);
+                    System.out.println(jsonNode.toString());
+                } catch (UnirestException e) {
+                    e.printStackTrace();
+                }
+            }
+            return emails;
         }
 
         return null;
