00ca6f0fcd4c8b3720e96e594a08a7e841776a6a
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-14 21:50:19 +0530

    import email Validator

--- src/main/java/authorization/EmailAuthorization.java
+++ src/main/java/authorization/EmailAuthorization.java
@@ -1,12 +1,14 @@
 package authorization;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
 import java.util.ArrayList;
 
 public class EmailAuthorization implements Authorization{
     ArrayList<String> newEmails = new ArrayList<String>();
 
     public ArrayList<String> Validation(ArrayList<String> emails){
-
+        EmailValidator validator = EmailValidator.getInstance();
 
         return newEmails;
     }
