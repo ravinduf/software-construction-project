ff046888db10ae9fca492d788ea203c52d5940b7
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-14 22:41:42 +0530

    add function to Read the database

--- src/main/java/emailRepository/MysqlEmailRepository.java
+++ src/main/java/emailRepository/MysqlEmailRepository.java
@@ -1,5 +1,9 @@
 package emailRepository;
 
+import java.util.ArrayList;
+
 public class MysqlEmailRepository implements EmailRepository {
-    
+    public ArrayList<String> ReadOperation(){
+        return null;
+    }
 }
