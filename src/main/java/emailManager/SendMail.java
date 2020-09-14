4e370fc79a75bab8c37efebbfb53e75a1f4ae21c
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-14 22:27:18 +0530

    add operation to SendMail class to send emails

--- src/main/java/emailManager/SendMail.java
+++ src/main/java/emailManager/SendMail.java
@@ -1,25 +1,24 @@
 package emailManager;
 
+import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
-import java.util.ArrayList;
-
 public class SendMail {
-    public ArrayList<String> EmailSendRepository(ArrayList<String> emails, String errorMessage) {
-        if ( errorMessage != null ) {
-            for (String email: emails ){
-                try {
-                    SendMail mailSend = new SendMail();
-                    JsonNode jsonNode = mailSend.sendSimpleMessage(email, errorMessage);
-                    System.out.println(jsonNode.toString());
-                } catch (UnirestException e) {
-                    e.printStackTrace();
-                }
-            }
-            return emails;
-        }
+    public JsonNode sendSimpleMessage(String email, String erroMessage) throws UnirestException {
+        String url = null;
+        String password = null;
 
-        return null;
+        HttpResponse<JsonNode> request = Unirest.post("https://api.mailgun.net/v3/" + "sandbox1578a10878f343abafdf04c79479b78b.mailgun.org" + "/messages")
+                .basicAuth("api", "1f39a178b73a57ce04dee76361879a98-0f472795-e19a09a9")
+                .field("from", "sachidisanayaka98@gmail.com")
+                .field("to", email)
+                .field("subject", "ERROR")
+                .field("text", erroMessage)
+                .asJson();
+
+        return request.getBody();
     }
+
 }
