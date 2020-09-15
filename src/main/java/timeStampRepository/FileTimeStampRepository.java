02b0bfcb57dccb53b7ac26e39ab290a20ccc9e56
bemaldvanitha <bdvidanagamachchi@gmail.com>
2020-09-15 11:06:13 +0530

    variables created

--- src/main/java/timeStampRepository/FileTimeStampRepository.java
+++ src/main/java/timeStampRepository/FileTimeStampRepository.java
@@ -1,5 +1,12 @@
 package timeStampRepository;
 
+import java.io.*;
+import java.util.*;
+
 public class FileTimeStampRepository {
+    FileInputStream fileInputStream;
+    InputStreamReader inputStreamReader;
+    BufferedReader bufferedReader;
+
 
 }
