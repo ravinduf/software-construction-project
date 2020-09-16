78010b06218f7f4ba057e3e992eecb997c76c86a
Chamodi Jayodya <jayodyachamodi@gmail.com>
2020-09-16 16:33:29 +0530

    Added codes to read the log file lines

--- src/main/java/repository/FileStringRepository.java
+++ src/main/java/repository/FileStringRepository.java
@@ -10,10 +10,30 @@ public class FileStringRepository implements StringRepository {
     BufferedReader bufferedReader;
     //string path2 = "src\\resources\\log.txt";
 
-    public ArrayList<String> FileRead(String path) {
+    public ArrayList<String> FileRead(String path) throws IOException {
+        String line;
+        ArrayList<String> readedData = new ArrayList<String>();
 
+        File file = new File(path);
 
+        try {
+            fileInputStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        inputStreamReader = new InputStreamReader(fileInputStream);
+        bufferedReader = new BufferedReader(inputStreamReader);
 
+        while ((line = bufferedReader.readLine())!= null) {
+            readedData.add(line);
+        }
+        fileInputStream.close();
+        inputStreamReader.close();
+        bufferedReader.close();
+        if (readedData != null ) {
+            return readedData;
+        }
+        //if the log file is empty
         return null;
     }
 }
--- src/main/java/repository/StringRepository.java
+++ src/main/java/repository/StringRepository.java
@@ -1,7 +1,8 @@
 package repository;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public interface StringRepository {
-    ArrayList<String> FileRead(String path);
+    ArrayList<String> FileRead(String path) throws IOException;
 }
