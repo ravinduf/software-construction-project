1ab6155ce403f05a94368314c4085f9dd68e0d03
Chamodi Jayodya <jayodyachamodi@gmail.com>
2020-09-16 16:08:19 +0530

    Set buffer readers

--- src/main/java/repository/FileStringRepository.java
+++ src/main/java/repository/FileStringRepository.java
@@ -1,11 +1,19 @@
 package repository;
 
+import java.io.*;
+
 import java.util.ArrayList;
 
 public class FileStringRepository implements StringRepository {
+    FileInputStream fileInputStream;
+    InputStreamReader inputStreamReader;
+    BufferedReader bufferedReader;
+    //string path2 = "src\\resources\\log.txt";
 
     public ArrayList<String> FileRead(String path) {
-        
+
+
+
         return null;
     }
 }
