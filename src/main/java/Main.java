7dcfab068c7b0c184bf887ecae65a1631442d90b
raavinduf <ravinduf@rocketmail.com>
2020-09-18 10:56:02 +0530

    finished main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,36 @@
+import Input.CommandLineInput;
+import authorization.Authorization;
+import authorization.EmailAuthorization;
+import emailManager.EmailHub;
+import emailRepository.EmailRepository;
+import emailRepository.MysqlEmailRepository;
+import operation.FilterOperation;
+import operation.Operation;
+import repository.FileStringRepository;
+import repository.StringRepository;
+import timeStampRepository.FileTimeStampRepository;
+import timeStampRepository.TimeStampRepository;
+import ui.CommandLineUI;
+import ui.UI;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Input input = new CommandLineInput();
+        StringRepository stringRepository = new FileStringRepository();
+        TimeStampRepository timeStampReposiroty = new FileTimeStampRepository();
+        Operation operation = new FilterOperation();
+        Operation operation1 = new FilterOperation();
+
+        EmailRepository emailRepository = new MysqlEmailRepository();
+        Authorization authorization = new EmailAuthorization();
+        EmailHub emailHub = new EmailHub();
+        UI ui = new CommandLineUI();
+        MailSendApp mailSendApp = new MailSendApp(input, stringRepository,  timeStampReposiroty, operation, operation1, timeStampReposiroty1,
+                emailRepository, authorization, emailHub, ui);
+        mailSendApp.Execute();
+
     }
 }
+
